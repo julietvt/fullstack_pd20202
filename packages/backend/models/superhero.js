f4fb58ecbc52d7416f12531cff8331dc428294a4
julietvt <freshitpt@gmail.com>
2021-05-22 11:40:25 +0300

    backend: model superhero was updated

--- packages/backend/models/superhero.js
+++ packages/backend/models/superhero.js
@@ -1,28 +1,37 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Superhero extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    static associate(models) {}
+  }
+  Superhero.init(
+    {
+      nickname: {
+        type: DataTypes.STRING,
+        require: true,
+        unique: true,
+      },
+      realName: {
+        type: DataTypes.STRING,
+        require: true,
+      },
+      originDescription: DataTypes.STRING,
+      superpowers: {
+        type: DataTypes.ARRAY(DataTypes.STRING),
+        defaultValue: [],
+      },
+      catchPhrase: DataTypes.STRING,
+      images: {
+        type: DataTypes.ARRAY(DataTypes.STRING),
+        defaultValue: [],
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Superhero',
+      underscored: true,
+      tableName: 'superheroes',
     }
-  };
-  Superhero.init({
-    nickname: DataTypes.STRING,
-    realName: DataTypes.STRING,
-    originDescription: DataTypes.STRING,
-    superpowers: DataTypes.ARRAY,
-    catchPhrase: DataTypes.STRING,
-    images: DataTypes.ARRAY
-  }, {
-    sequelize,
-    modelName: 'Superhero',
-  });
+  );
   return Superhero;
 };
