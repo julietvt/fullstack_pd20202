f6ad72254a52f644fd840ab457f903865e72be8d
julietvt <freshitpt@gmail.com>
2021-05-22 14:01:58 +0300

    backend: uploadImages.mw

--- packages/backend/middleware/uploadImages.mw.js
+++ packages/backend/middleware/uploadImages.mw.js
@@ -1,3 +1,22 @@
 const multer = require('multer');
 const path = require('path');
+const { staticPath } = require('./../config/config');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(staticPath, '/images'));
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '_' + file.originalname);
+  },
+});
+
+const MIME_REGEXP = /^image\/(jpeg|png|gif)/;
+
+function fileFilter(req, file, cb) {
+  cb(null, MIME_REGEXP.test(file.mimetype));
+}
+
+const upload = multer({ storage: storage, fileFilter: fileFilter });
+
 module.exports = upload;
